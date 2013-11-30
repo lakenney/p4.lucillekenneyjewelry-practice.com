cc59969a953c7d1fb6b2fd98699d98b41877d499
Lucille Kenney <lucilleakenney@gmail.com>
2013-11-30 15:07:38 -0500

    Copied from p2

---
+++ views/v_posts_add.php
@@ -0,0 +1,11 @@
+<!-- View to add a post -->
+
+	<form method='POST' action='/posts/p_add'>
+    	<label for='content'>New Post</label><br>
+    	<textarea  cols="53" rows="8" name='content' id='content'></textarea>
+    
+    	<br><br>
+    	<input type='submit' value='New Post'>
+
+	</form>
+
